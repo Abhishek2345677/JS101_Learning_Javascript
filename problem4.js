fa541592590b9a8fe3c983337127e28f4d27f5d8
Abhishek <abhishekjain34566@gmail.com>
2022-09-08 21:49:55 +0530

    I am pushig string problem4
    I am pushig string code
    
     git commit -m "I am pushig string code"

---
+++ problem4.js
@@ -0,0 +1,11 @@
+//password should be at least of 6 char
+let password = "masaihello"
+
+if(password.length <6)
+{
+  console.log("warning : choose a password equal to or more than 6 character")
+}
+else 
+{
+    console.log("Sign in  successfully")
+}
